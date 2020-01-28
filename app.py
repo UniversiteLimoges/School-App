5c680c01cd5d0b439af6aa4f4e9e32cec7203a00
Thibault Magy <magy.thibault@gmail.com>
2020-01-28 14:53:53 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,33 @@
+# ------------------------------------ CLASSES --------------------------------------
+# TODO: create and finish the classes
+# TODO: refactor into multiple files
+class Etudiant(object):
+    pass
+
+class Cours(object):
+    pass
+
+class Notes(object):
+    pass
+
+# -------------------------------------- MAIN ----------------------------------------
+# TODO: create and finish the main function
+def main():
+    pass
+
+# -------------------------------------- FUNCTIONS ----------------------------------------
+def print_menu():
+    print("--------------------------------------------------------")
+    print("1 - Lecture des donnees depuis un fichier")
+    print("2 - Ajouter un etudiant")
+    print("3 - Ajouter une note")
+    print("4 - Afficher les notes d'un etudiant")
+    print("5 - Afficher les notes triees d'un cours")
+    print("6 - Supprimer un cours")
+    print("7 - Sauvegarder des donnees dans un fichier")
+    print("8 - Quitter")
+    print("\n")
+
+# -------------------------------------- __MAIN__ ----------------------------------------
+def __main__():
+    main()
