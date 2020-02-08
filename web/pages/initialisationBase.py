2d496cb2c959291b34f231d541d2519f4aa53849
Thibault Magy <magy.thibault@gmail.com>
2020-02-08 15:16:18 +0100

    Creation de la base

--- web/pages/initialisationBase.py
+++ web/pages/initialisationBase.py
@@ -12,19 +12,19 @@ print(header)
 
 # Parameters
 tony = {"login": "root", "password": "", "database": "" }
+thibault = {"login": "admin", "password": "", "database": "test" }
 default = {"login": "test", "password": "test", "database": "ruches" }
 
 class PythonSqlConnect(object):
-    cursor = None
 
     def __init__(self, param = None):
         # Open database connection
-        connection = pymysql.connect('localhost', param["login"], param["password"], param["database"])
+        self.connection = pymysql.connect('localhost', param["login"], param["password"], param["database"])
         # Open database connection
         # connection = pymysql.connect('localhost', param["login"], param["password"], param["database"])
 
         # Cursor
-        self.cursor = connection.cursor()
+        self.cursor = self.connection.cursor()
 
     # Get the version of the database
     def version(self):
@@ -33,13 +33,53 @@ class PythonSqlConnect(object):
         print("Database version: {}".format(version[0]))
 
     # Create a database
-    def createDatabase(table):
+    def createDatabase(self, table):
         self.cursor.execute("DROP TABLE IF EXISTS " + table)
         sqlQuery = "CREATE DATABASE " + table
         self.cursor.execute(sqlQuery)
         print("Database {} crée avec succès".format(table))
 
-    def displayDatabases(self):
+    def createTables(self):
+        self.cursor.execute("DROP TABLE IF EXISTS Notes")
+        self.cursor.execute("DROP TABLE IF EXISTS Students")
+        self.cursor.execute("DROP TABLE IF EXISTS Courses")
+        sql1 = """
+            CREATE TABLE Students
+            (
+               	student_id INT PRIMARY KEY NOT NULL,
+                prenom VARCHAR(100),
+                nom VARCHAR(100),
+                age INT
+            );
+        """
+        sql2 ="""
+            CREATE TABLE Courses
+            (
+                course_id INT PRIMARY KEY NOT NULL,
+                nom VARCHAR(100),
+                annee INT
+            );
+        """
+        sql3 = """
+            CREATE TABLE Notes
+           (
+               note_id INT PRIMARY KEY NOT NULL,
+               note INT,
+               student_id INT,
+               course_id INT,
+               FOREIGN KEY (student_id) REFERENCES Students(student_id),
+               FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+           );
+        """
+        self.cursor.execute(sql1)
+        self.cursor.execute(sql2)
+        self.cursor.execute(sql3)
+        print("Tables créées")
+
+    def populateDatabase(self, table):
+        pass
+
+    def displayDatabases(self, table):
         # SQL query string
         sqlQuery = "SHOW DATABASES"
     
@@ -52,26 +92,29 @@ class PythonSqlConnect(object):
         for database in databaseList:
             print(database)
 
+    def closeCon(self):
+        self.connection.close()
+
 try:
-    con = PythonSqlConnect(tony)
+    con = PythonSqlConnect(thibault)
     con.version()
+    # con.createDatabase('test')
+    con.createTables()
+    con.displayDatabases('test')
+    con.closeCon()
+
     
     # db_table = "test"
     # connection = pymysql.connect('localhost', "root", "")
-    # cursor = connection.cursor()
-    # sqlStatement = "CREATE DATABASE TEST"
-    # cursor.execute(sqlStatement)
+    # with con.cursor() as cursor:
+    #     sql = "CREATE DATABASE TEST"
+    #     cursor.execute(sql)
 
-    # con.createDatabase()
 
-    #createDatabase(cursor, table)
-
- 
-        
 except Exception as e:
     print("Exeception occured:{}".format(e))
 
 finally:
     # disconnect from server
-    con.close()
+    print("DONE")
 
